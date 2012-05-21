60ac218f2a168931b4bcb56515361009fe749cd5
Jaroslav Kvapil <begbang@gmail.com>
2012-05-21 17:27:25 +0200

    Dokoncen web projekt

--- LibraryWebProject_new/src/com/ibm/library/servlets/ProcessListItems.java
+++ LibraryWebProject_new/src/com/ibm/library/servlets/ProcessListItems.java
@@ -16,7 +16,9 @@ import javax.servlet.http.HttpSession;
 import com.ibm.ils.library.datastore.exceptions.SystemUnavailableException;
 import com.ibm.ils.library.model.Copy;
 import com.ibm.ils.library.model.LoanedCopy;
+import com.ibm.ils.library.model.Patron;
 import com.ibm.ils.library.model.exceptions.OperationFailed;
+import com.ibm.ils.library.model.exceptions.PatronNotFound;
 import com.ibm.library.beans.LoanedCopyListBean;
 
 /**
@@ -43,22 +45,14 @@ public class ProcessListItems extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(false);
 		
-		int patronId = -1;
-		try {
-			patronId = Integer.parseInt(session.getAttribute("patron").toString());
-		} catch (NumberFormatException e) {
-			showErrorMessage("Error: Patron ID is in bad format!", request, response);
-		} catch (NullPointerException e) {
-			showErrorMessage("Error: You are not logged in!", request, response);
-		}
-		
+		int patronId = getPatronId(request, response, session);
 		
 		ServletContext context = getServletContext();
 		Copy copyX = new Copy();
 		Collection<LoanedCopy> loanedCopyList = null;
 		try {
 			loanedCopyList = copyX.findLoanedCopiesForPatronId(patronId);
-			request.setAttribute("copyList", loanedCopyList);
+			session.setAttribute("copyList", loanedCopyList);
 		} catch (SystemUnavailableException e) {
 			showErrorMessage("Error: System is unavailable!", request, response);
 		} catch (OperationFailed e) {
@@ -77,6 +71,18 @@ public class ProcessListItems extends HttpServlet {
 		
 		
 	}
+	
+	protected int getPatronId(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws ServletException, IOException {
+		try {
+			return Integer.parseInt(session.getAttribute("patron").toString());
+		} catch (NumberFormatException e) {
+			showErrorMessage("Error: Patron ID is in bad format!", request, response);
+		} catch (NullPointerException e) {
+			showErrorMessage("Error: You are not logged in!", request, response);
+		}
+		
+		return 0;
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
@@ -84,9 +90,26 @@ public class ProcessListItems extends HttpServlet {
 	 */
 	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
 		String[] checkBoxes = request.getParameterValues("renew");
 		
-		Collection<LoanedCopy> loanedCopyList = (Collection<LoanedCopy>)request.getAttribute("copyList");
+		int patronId = getPatronId(request, response, session);
+		
+		Patron patron = null;
+		try {
+			patron = Patron.findById(patronId);
+		} catch (PatronNotFound e) {
+			// TODO Automaticky generovaný blok catch
+			e.printStackTrace();
+		} catch (SystemUnavailableException e) {
+			// TODO Automaticky generovaný blok catch
+			e.printStackTrace();
+		} catch (OperationFailed e) {
+			// TODO Automaticky generovaný blok catch
+			e.printStackTrace();
+		}
+		
+		Collection<LoanedCopy> loanedCopyList = (Collection<LoanedCopy>)session.getAttribute("copyList");
 		
 		if (checkBoxes != null) {
 			for( String s : checkBoxes ) {
@@ -94,9 +117,27 @@ public class ProcessListItems extends HttpServlet {
 				for (int a = 1; a < Integer.parseInt(s); a++) {
 					i.next();
 				}
-				LoanedCopy copy = (LoanedCopy)i.next();
-				copy.setRenewRequest(true);
-				System.out.println( "<br />checkBox: "+s );
+				
+				i.next().setRenewRequest(true);
+			}
+			
+			try {
+				patron.renew(loanedCopyList);
+				
+				String info = "";
+				for (LoanedCopy copy : loanedCopyList) {
+					if (copy.isRenewRequest()) {
+						info += copy.getRenewMessage() + "<br />";
+					}
+				}
+				session.setAttribute("info", info);
+				response.sendRedirect("Default");
+			} catch (OperationFailed e) {
+				// TODO Automaticky generovaný blok catch
+				e.printStackTrace();
+			} catch (SystemUnavailableException e) {
+				// TODO Automaticky generovaný blok catch
+				e.printStackTrace();
 			}
 		}
 		
